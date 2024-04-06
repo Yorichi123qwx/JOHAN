3febc1f1c8acde0efbb8f5a4fd07db2138e9788c
Yorichi123qwx <164789522+Yorichi123qwx@users.noreply.github.com>
2024-04-06 12:10:57 +0200

    Update رد تلقائي بصوره.js

--- plugins/رد تلقائي بصوره.js
+++ plugins/رد تلقائي بصوره.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
     let taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
-    let message = `ماذا تريد`;
+    let message = `أهلا انا يوريتشي.... ماذا تريد؟ `;
 
     conn.sendFile(m.chat, 'https://telegra.ph/file/26833081b921684bbc73a.jpg', 'image.jpg', message, m);
 };
